61e8adc5c269c464f0d0608b163991ac43afea61
janardhanmerugu <janardhanmerugu@gmail.com>
2025-11-01 05:29:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-// Your Firebase config
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCmaZ_ojXb5Xkg9b5pu4ng0WaNzw42BEwc",
+  authDomain: "dhanda-c6f81.firebaseapp.com",
+  databaseURL: "https://dhanda-c6f81-default-rtdb.firebaseio.com",
+  projectId: "dhanda-c6f81",
+  storageBucket: "dhanda-c6f81.firebasestorage.app",
+  messagingSenderId: "1078087902179",
+  appId: "1:1078087902179:web:7291c62f89528e732176d6",
+  measurementId: "G-QG764Y58JM"
 };
 
 // Initialize Firebase
@@ -25,3 +26,4 @@ onValue(dataRef, (snapshot) => {
   const data = snapshot.val();
   document.getElementById("data").textContent = JSON.stringify(data, null, 2);
 });
+
