58d5dd6488da7d2825afb628b946f022e4add100
janardhanmerugu <janardhanmerugu@gmail.com>
2025-11-03 19:47:30 +0530

    dsf

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ onAuthStateChanged(auth, user => {
   if (user) {
     userInfo.textContent = `Signed in as ${user.displayName}`;
     loginBtn.style.display = "none";
+    logoutBtn.style.display = "inline-block";
     dataSection.style.display = "flex";
     loadUsers();
   } else {
